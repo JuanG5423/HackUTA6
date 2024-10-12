b355e03cf11b84f191aac22e8fe984cfbaf639c0
Raia <arefeen.nafi@gmail.com>
2024-10-12 17:28:46 -0500

    encription key for testing, Page type format and assetsgit add .

---
+++ Pages/Home.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from PIL import Image
+
+image_path = "Assets/JADALOGO.png"
+# with col2:
+#     st.image(image_path, width=450)
+col1, col2, col3 = st.columns([1, 2, 1])
+
+with col2:
+    # Dynamically set image width based on perceived container width
+    st.image(image_path, use_column_width=True);  # Full width for smaller screens
+   
+   
+st.text(
+    "JADA is a Journaling AI Detection App. Anger, joy, sadness, write your mind down and\nsee what JADA thinks"
+    )
+    
+    
---
+++ Pages/Journal.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import os
+from cryptography.fernet import Fernet
+
+
+with open('encryption_key.key', 'rb') as key_file:
+    key = key_file.read()
+    
+f = Fernet(key)
+
+col1, center, col3 = st.columns([2, 2, 2])
+with center:
+    folder_path = './entries'
+    for file_name in os.listdir(folder_path):
+        # Check if the file has a .jada extension
+        if file_name.endswith('.jada'):
+            file_path = os.path.join(folder_path, file_name)
+            with open(file_path, 'r') as file:
+                context = f.decrypt(file.read().encode('utf-8')).decode()
+                container = st.container(border=True)
+                container.columns(len(os.listdir(folder_path)), vertical_alignment="bottom")
+                container.write(context)
+
+    add_page = st.container()
+    if add_page.button("Add Page",icon=":material/add_circle:"):
+        st.switch_page("Page.py")

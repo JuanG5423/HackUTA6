6a8ebb25d86ef08ddbc99f032088a7ce3ddc9dae
Abdulmuizz Wahab <wahababdulmuizz85@gmail.com>
2024-10-12 17:18:32 -0500

    my stuff

---
+++ Pages/Journals.py
@@ -0,0 +1,46 @@
+import streamlit as st  
+import os
+from cryptography.fernet import Fernet
+
+st.title("Write Your Mind")
+
+# key = Fernet.generate_key()
+# with open('encryption_key.key', 'wb') as key_file:
+#     key_file.write(key)
+# STORING KEYS(WILL BE CHANGED SYSTEM)
+
+with open('encryption_key.key', 'rb') as key_file:
+    key = key_file.read()
+    
+f = Fernet(key)
+
+
+st.title("Journay Entry 1:?")
+entry = st.text_area("write your feelings", height = 900)
+
+with open("entries/entry.jada", "w") as file:
+    file.write(f.encrypt(entry.encode('utf-8')).decode('utf-8'))
+    file.write("\n")
+    
+
+
+with open('encryption_key.key', 'rb') as key_file:
+    key = key_file.read()
+    
+f = Fernet(key)
+
+
+folder_path = './entries'
+for file_name in os.listdir(folder_path):
+    # Check if the file has a .jada extension
+    if file_name.endswith('.jada'):
+        file_path = os.path.join(folder_path, file_name)
+        with open(file_path, 'r') as file:
+            context = f.decrypt(file.read().encode('utf-8')).decode()
+
+            container = st.container()
+            container.columns(3, vertical_alignment="bottom")
+            container.caption(context)
+
+
+st.button("Add Page",icon=":material/add_circle:")
--- main.py
+++ main.py
@@ -1,19 +1,89 @@
-import streamlit as st
+import streamlit as st 
+from PIL import Image 
 
-st.title("Your App Title")
-st.write("Description or main content here")
 
-#Make journal feature where you can write your thoughts and save them
-st.write("Journal Feature")
-journal = st.text_area("Write your thoughts here")
-if st.button("Save"):
-    with open("journal.txt", "a") as file:
-        file.write(journal + "\n")
-    st.write("Saved successfully")
+# -- Page setup -- 
+# st.set_page_config(
+#     page_title= "JADA APP",
+#     page_icon= "JADAAI.png",
+# )
 
-#Make a feature to view your saved journal entries
-st.write("View Journal Entries")
-with open("journal.txt", "r") as file:
-    journal_entries = file.readlines()
-    for entry in journal_entries:
-        st.write(entry)
+about_page = st.Page(
+    page ="Pages/Aboutus.py",
+    title="About us",
+    icon =":material/groups:",
+   
+)
+
+journal_page = st.Page(
+    page = "Pages/Journals.py",
+    title = "Journals",
+    icon = ":material/book:",
+)
+
+home_page = st.Page(
+    page = "Pages/Home.py",
+    title = "Home",
+    icon = ":material/nest_eco_leaf:",
+    default= True,
+)
+
+Demo_page = st.Page(
+    page = "Pages/Demo.py",
+    title = "Demo",
+    icon = ":material/play_arrow:",
+)
+
+
+
+
+# --- Navigation setup [without sections] --
+
+st.logo("/home/unknown/HackUTA6/Assets/JADALOGO.png") 
+NPG = st.navigation(pages=[about_page, journal_page, home_page, Demo_page])
+NPG.run()
+
+
+
+
+# st.markdown(
+#         "<h1 style='text-align: center;'>WELCOME</h1>", 
+#         unsafe_allow_html=True
+# #     )
+# image_path = "Assets/JADALOGO.png"
+# with col2:
+#     st.image(image_path, width=450)
+    
+
+# Logo_Image = Image.open("Assets/JADALOGO.png")
+# st.image(
+#     Logo_Image,
+#     caption = "JADA is a Journaling AI Detection App. Anger, joy, sadness, write your mind down and see what JADA thinks",
+#     width= 900,
+#     #channels= "RGB"
+# )
+
+
+# Display and center the image
+
+
+# markdown to create a container that centers the image
+# st.markdown(
+#     f"""
+#     <div style="display: flex; justify-content: center;">
+#         <img src="{image_path}" alt="centered image" width="300">
+#     </div>
+#     """,
+#     unsafe_allow_html=True
+# )
+# Local image path
+
+# Create two empty columns with one column in the center
+
+
+# Center the image in the middle column
+
+
+#st.image(image_path, width=800)
+#st.text("J-A-D-A is a journaling AI Detection App. Anger, joy, sadness, write your mind down and see how jada interprets it.\n")
+#st.markdown("![Hello](Assets/JADAAI.png)")

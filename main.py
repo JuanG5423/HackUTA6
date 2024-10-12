6847c9195abeeed404f022b20cd532539d4608ce
Raia <arefeen.nafi@gmail.com>
2024-10-12 12:52:10 -0500

    baseline commit testing

--- main.py
+++ main.py
@@ -17,3 +17,11 @@ with open("journal.txt", "r") as file:
     journal_entries = file.readlines()
     for entry in journal_entries:
         st.write(entry)
+
+row1 = st.columns(3)
+row2 = st.columns(3)
+
+for col in row1 + row2:
+    tile = col.container(height=120)
+    tile.title(":balloon:")
+    tile.write("journal stuff")

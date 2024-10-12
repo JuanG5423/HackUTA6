0b5a1da7a493b02be2f51b099f276cb01c7ef8aa
JuanG5423 <guajardo5423@gmail.com>
2024-10-12 12:52:49 -0500

    commit to Juan branch

--- ai.py
+++ ai.py
@@ -1,13 +1,16 @@
 from transformers import pipeline
 
 # Load sentiment analysis model
-sentiment_analyzer = pipeline("sentiment-analysis")
+#sentiment_analyzer = pipeline("sentiment-analysis")
 
 # Analyze sentiment of a text
-result = sentiment_analyzer("I'm feeling fantastic today!")
-print(result)  # Output: [{'label': 'POSITIVE', 'score': 0.9998}]
+#result = sentiment_analyzer("I'm feeling fantastic today!")
+#print(result)  # Output: [{'label': 'POSITIVE', 'score': 0.9998}]
+
+
+print("This is in branch Juan")
 
 # For emotion detection
 emotion_analyzer = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base")
-emotions = emotion_analyzer("I feel so stressed and anxious about my work.")
+emotions = emotion_analyzer("My friend went to the beach and gotgot a tan. Good on them")
 print(emotions)

26eaf8de98a48e3cd1b43c1ea8d322526a367016
Raia <arefeen.nafi@gmail.com>
2024-10-12 13:55:16 -0500

    begining of encryption for journals

--- Page.py
+++ Page.py
@@ -1,4 +1,13 @@
 import streamlit as st
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key()
+f = Fernet(key)
+
+# f.encrypt(entry.encode('utf-8')).decode('utf-8')
 
 st.title("How Are You Feeling Today?")
-st.text_area("")
+entry = st.text_area("write your feelings", height = 900)
+
+with open("entries/entry.jada", "w") as file:
+    file.write(entry + "\n")

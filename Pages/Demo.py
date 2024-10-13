8a22275fdcb196d480d46c848958b451de4f11ef
JuanG5423 <guajardo5423@gmail.com>
2024-10-13 00:37:09 -0500

    Trying to fix streamlit issues

--- Pages/Demo.py
+++ Pages/Demo.py
@@ -3,12 +3,12 @@ import streamlit as st
 st.write("Journal Feature")
 journal = st.text_area("Write your thoughts here")
 if st.button("Save"):
-    with open("/home/unknown/HackUTA6/Pages/JournalDB/journal.txt", "a") as file:
+    with open("Pages/JournalDB/journal.txt", "a") as file:
         file.write(journal + "\n")
     st.write("Saved successfully")
     
     st.write("View Journal Entries")
-with open("/home/unknown/HackUTA6/Pages/JournalDB/journal.txt", "r") as file:
+with open("Pages/JournalDB/journal.txt", "r") as file:
     journal_entries = file.readlines()
     for entry in journal_entries:
         st.write(entry)
